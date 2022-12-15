c661b221784edf7457e101afc6027b4c0d5d04b2
salva <salroiba03@gmail.com>
2022-12-15 17:14:57 +0100

    Menu migrations

---
+++ backend/laravel/database/migrations/2022_12_15_155745_create_table_menu.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id("id");
+            $table->timestamps();
+            $table->string("dish")->unique();
+            $table->foreignId('type')->constraint('dish_type');
+            $table->float("price");
+            $table->string("desc");
+            $table->string("photo");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+};
