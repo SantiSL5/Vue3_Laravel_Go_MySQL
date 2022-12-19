becdfac5662011cce8d456bc6507042ffbb2fdcb
salva <salroiba03@gmail.com>
2022-12-19 18:00:36 +0100

    Bootstrap js

--- frontend/vue3/src/main.js
+++ frontend/vue3/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import './main.css';
 import App from './App.vue'
 
