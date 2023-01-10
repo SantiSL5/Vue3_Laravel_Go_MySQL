5ba32fd90106dff76f46c0ece8b3aa080bb6a0d3
SantiSL5 <santiagosolerllin@gmail.com>
2023-01-10 17:57:56 +0100

    Entrypoint laravel modified

--- build/entrypointlaravel.sh
+++ build/entrypointlaravel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 chown -R laravel:laravel /var/www
 composer install
+php artisan key:generate
+php artisan route:list
+php artisan vendor:publish --provider="PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider"
+php artisan jwt:secret
 php artisan migrate:status
 php artisan migrate
 php artisan serve --host=0.0.0.0 --port=6001
